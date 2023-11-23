9f4da81beb11ab5dc2e94b21590860c8434496a2
nfcdjobo <nfcdjobo@gmail.com>
2023-11-23 20:11:36 +0000

    Mise à jours des url de rédirection

--- corporates/scriptAdmin/deconnexion.js
+++ corporates/scriptAdmin/deconnexion.js
@@ -9,22 +9,22 @@ window.addEventListener(`DOMContentLoaded`, (e)=>{
         if(localStorage.SESSION_TRANSPORT){
             const local=JSON.parse(localStorage.SESSION_TRANSPORT).body.user.code;
             if(confirm('Êtes-vous sûre de vouloir fermer votre session ?')){
-                 localStorage.removeItem('SESSION_TRANSPORT');
+                localStorage.removeItem('SESSION_TRANSPORT');
                 if(location.href.includes('index.html')){
                     if(local.includes('ADMIN')){
-                        location.href='corporates/login.html';
+                        location.href='./corporates/login.html';
                     }else if(local.includes('PASSA')){
-                        location.href='corporates/login2.html';
+                        location.href='./corporates/login2.html';
                     }else{
-                        location.href='corporates/login3.html';
+                        location.href='./corporates/login3.html';
                     }
                 }else{
                     if(local.includes('ADMIN')){
-                        location.href='login.html';
+                        location.href='./login.html';
                     }else if(local.includes('PASSA')){
-                        location.href='login2.html';
+                        location.href='./login2.html';
                     }else{
-                        location.href='login3.html';
+                        location.href='./login3.html';
                     }
                 }
             }
@@ -33,19 +33,20 @@ window.addEventListener(`DOMContentLoaded`, (e)=>{
 
 
     if(!localStorage.SESSION_TRANSPORT){
+        
         if(location.href.includes('index.html')){
             deconnexionConnexionRef.forEach(item=>item.innerHTML=`<a href="#">Se connecter</a>
             <ul class="sub-menu">
-                <li><a href="corporates/login.html">Administrateur</a></li>
-                <li><a href="corporates/login2.html">Conducteur</a></li>
-                <li><a href="corporates/login3.html">Client</a></li>
+                <li><a href="./corporates/login.html">Administrateur</a></li>
+                <li><a href="./corporates/login2.html">Conducteur</a></li>
+                <li><a href="./corporates/login3.html">Client</a></li>
             </ul>`);
         }else{
             deconnexionConnexionRef.forEach(item=>item.innerHTML=`<a href="#">Se connecter</a>
             <ul class="sub-menu">
-                <li><a href="login.html">Administrateur</a></li>
-                <li><a href="login2.html">Conducteur</a></li>
-                <li><a href="login3.html">Client</a></li>
+                <li><a href="./login.html">Administrateur</a></li>
+                <li><a href="./login2.html">Conducteur</a></li>
+                <li><a href="./login3.html">Client</a></li>
             </ul>`);
         }
 
